61013ff2d21e1baf2d9cbb7e7a27ee49e39994f2
Laboratorio Computación N° 1 <claseslab1@inacap.cl>
2026-06-04 16:32:22 -0400

    Estructura del proyecto inicial

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "../pages/Home"
+import Login from "../pages/Login"
+import UserDashboard from "../pages/user/UserDashboard"
+import CoachDashboard from "../pages/coach/CoachDashboard"
+import AdminDashboard from "../pages/admin/AdminDashboard"
+
+import UserLayout from "../layouts/UserLayouts"
+import CoachLayout from "../layouts/CoachLayouts"
+import AdminLayout from "../layouts/AdminLayouts"
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                
+                <Route path="/user" element={<UserLayout />}>
+                    <Route path="dashboard" element={<UserDashboard />} />
+                </Route>
+                <Route path="/coach" element={<CoachLayout />}>
+                    <Route path="dashboard" element={<CoachDashboard />} />
+                </Route>
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route path="dashboard" element={<AdminDashboard />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default AppRoutes
